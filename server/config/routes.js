d07c71bad1cc51aabaf4295c947cf6ffaba2700b
Todd MacIntyre <ToddCMacIntyre@gmail.com>
2017-05-13 18:45:26 -0400

    start building nexmo

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,4 +1,8 @@
+const nexmo = require('../controllers/nexmo.js');
 
 module.exports = (app) => {
-  
+  app.post('/nexmo', function(req, res) {
+    nexmo.test(req);
+    res.send('worked');
+  });
 }
