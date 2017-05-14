f4aa77769526ceabb8e0a14090374fd0f515c0c8
Madelena Mak <madelena+github@outlook.com>
2017-05-14 01:34:06 -0400

    Split muiTheme out as style.jsx

---
+++ src/shared/style.jsx
@@ -0,0 +1,21 @@
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+export const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: '#EA750A',
+    // primary2Color: cyan700,
+    // primary3Color: grey400,
+    accent1Color: '#FF0055',
+    // accent2Color: grey100,
+    // accent3Color: grey500,
+    // textColor: darkBlack,
+    // secondaryTextColor: fade(darkBlack, 0.54),
+    // alternateTextColor: white,
+    // canvasColor: white,
+    // borderColor: grey300,
+    // disabledColor: fade(darkBlack, 0.3),
+    // pickerHeaderColor: cyan500,
+    // clockCircleColor: fade(darkBlack, 0.07),
+    // shadowColor: fullBlack,
+  },
+});
