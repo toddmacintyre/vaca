eac4318a2d2f4532e9d35649b94b6654c7b504ba
Madelena Mak <madelena+github@outlook.com>
2017-05-14 08:13:36 -0400

    Super Fancy Splash Screen

--- build/splash.js
+++ build/splash.js
@@ -1,8 +1,13 @@
 document.getElementById("splash").addEventListener("click", function(){
-  document.getElementById("splash").className = "hide-splash";
+  // document.getElementById("splash").className = "hide-splash";
+  document.getElementById("splash-1").className = "hide-splash-div";
+  document.getElementById("splash-2").className = "hide-splash-div";
+  document.getElementById("splash-3").className = "hide-splash-div";
+  document.getElementById("splash-4").className = "hide-splash-div2";
+
   window.setTimeout(function() {
     document.getElementById("splash").remove();
-  },1000);
+  },4000);
 });
 if(window.location.href !== "http://localhost:1337/home" && window.location.href !== "http://localhost:1337" && window.location.href !== "http://localhost:1337/") {
     document.getElementById("splash").remove();
