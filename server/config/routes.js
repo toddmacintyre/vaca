fd18a126085267083c48b6c8922a8f9a55112bee
Todd MacIntyre <ToddCMacIntyre@gmail.com>
2017-05-13 21:38:02 -0400

    send sms messages with nexmo

--- server/config/routes.js
+++ server/config/routes.js
@@ -3,14 +3,7 @@ const env = require('../../env.json');
 const nexmoKey = env.nexmoKey;
 const nexmoSecret = env.nexmoSecret;
 
-// const object = {
-// 	"from": "12035338310",
-// 	"to": "16026707502",
-// 	"type": "text",
-// 	"text": "Hello there!",
-// 	"api_key": env.nexmoKey,
-// 	"api_secret": env.nexmoSecret,
-// }
+
 
 module.exports = (app) => {
   app.post('/nexmo', function(req, res) {
--- server/controllers/nexmo.js
+++ server/controllers/nexmo.js
@@ -1,6 +1,33 @@
-const test = (text) => {
-  return `${text}!!`;
-}
+const env = require('../../env.json');
+const axios = require('axios');
 
+const test = (text) => {
+  console.log('text', text);
+
+  const makeRequest = false; // change this back to true to start making api calls again
+
+  const reqBody = {
+    "from": "16026707502",
+    "to": "16026707502",
+    "type": "text",
+    "text": text,
+    "api_key": env.nexmoKey,
+    "api_secret": env.nexmoSecret,
+  }
+
+
+  if (makeRequest === 'sms') {
+    return axios.post('https://rest.nexmo.com/sms/json', reqBody)
+      .then(function (response) {
+        return `success: ${response}`;
+      })
+      .catch(function (error) {
+        return `fail: ${error}`;
+      });
+  } else if (makeRequest === 'phone') {
+    return;
+  }
+  return 'makeRequest set to false.';
+}
 
 module.exports = { test, };
