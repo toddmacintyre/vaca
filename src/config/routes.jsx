cdaf3985d7f4d32d86be06387a25e4b061403be1
Todd MacIntyre <ToddCMacIntyre@gmail.com>
2017-05-13 23:03:07 -0400

    create tripItinerary.jsx

--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-d
 
 import Home from '../components/home';
 import PaymentDetails from '../components/paymentDetails';
+import TripItinerary from '../components/tripItinerary.jsx';
 import Test from '../components/test';
 
 class Routes extends Component {
@@ -11,6 +12,7 @@ class Routes extends Component {
       <Router>
         <Switch>
           <Route path="/paymentdetails" component={PaymentDetails} />
+          <Route path="/tripItinerary" component={TripItinerary} />
           <Route path="/test" component={Test} />
           <Route exact path="/" render={() => <Redirect to="/home" /> } />
           <Route path="/home" component={Home} />
