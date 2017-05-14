c3ba4cbc5c7148295f523609526df9d3910e6eb1
Todd MacIntyre <ToddCMacIntyre@gmail.com>
2017-05-13 20:39:02 -0400

    create nexmo controller

--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import Login from '../components/login';
+import Test from '../components/test';
 
 class Routes extends Component {
   render() {
@@ -10,6 +11,7 @@ class Routes extends Component {
         <Switch>
           <Route exact path="/" render={() => <Redirect to="/login" /> } />
           <Route path="/login" component={Login} />
+          <Route path="/test" component={Test} />
         </Switch>
       </Router>
     )
